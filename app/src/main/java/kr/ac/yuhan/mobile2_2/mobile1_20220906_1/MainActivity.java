196b1c255760b3404b78bcf4b038aa833ad94bbe
LIM <aosj2131@naver.com>
2022-09-06 14:57:15 +0900

    Initial commit

---
+++ app/src/main/java/kr/ac/yuhan/mobile2_2/mobile1_20220906_1/MainActivity.java
@@ -0,0 +1,14 @@
+package kr.ac.yuhan.mobile2_2.mobile1_20220906_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
